d25e9463301b60ff45cf9935f57ecfea828b8cd7
bankenhack <124228454+bankenhack@users.noreply.github.com>
2026-08-05 21:03:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 示例：添加第三方包源（按需取消注释）
 git clone https://github.com/kenzok8/openwrt-packages.git package/kenzok-packages
+# git clone https://github.com/kenzok8/small.git package/small
